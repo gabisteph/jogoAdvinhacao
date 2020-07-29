3257ccc3f3896e7a7675eac13931f8bb97783cdf
gabisteph <gabriellepiresmestrinho@gmail.com>
2020-07-29 17:18:57 -0400

    3/4 curso

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(){
     //imprime cabelhacalho do nosso jogo
@@ -6,13 +9,51 @@ int main(){
     printf("*bem vindo ao nosso jogo de adivinhação*\n");
     printf("**************************************\n");
     
-    int numerosecreto = 42;
+    int segundos = time(0);
+    srand(segundos);
+
+    int numerogrande = rand();
+
+    int numerosecreto = numerogrande % 100;
     int chute;
+    int ganhou = 0;
+    int tentativas = 1;
+    double pontos = 1000;
+    
+    
 
-    printf("Qual é o seu chute?");
-    scanf("%d", &chute);
+    while(ganhou == 0) {
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual é o seu chute?\n");
+        scanf("%d", &chute);
 
-    printf("Seu chute foi %d", chute);
+        
+        int acertou = (chute == numerosecreto);
+        int maior = (chute > numerosecreto);
+        if(chute < 0) {
+            printf("Você não pode chutar números negativos!\n");
+            continue;
+        }
+        if(acertou){
+            printf("Parabens! Voce acertou!\n");
+            printf("Jogue de novo, você é um bom jogador\n");
+            ganhou = 1;
+            break;
+        }
+        else if(maior){
+            printf("Seu chute foi maior que o número secreto\n");
+        }
+        else{
+                printf("Seu chute foi menor que o número secreto\n");
+            }
+        tentativas = tentativas + 1;
+        double pontosperdidos = abs(chute - numerosecreto) / (double)2;
+        pontos = pontos - pontosperdidos;
+    }
+    printf("FIM DO JOGO!\n");
+    printf("Total de pontos: %.f\n", pontos);
+    printf("Você acertou em %d tentativas!\n", tentativas);
+
 }
 
 //anotações da aula
---
+++ adivinhacao/teste.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	scanf("%d", &a);
+	for(int i=0;i=1;i++){
+        int mult = a*i;
+		printf("%d x %d = %d\n", a, i, a*i);
+    }
+
+}
